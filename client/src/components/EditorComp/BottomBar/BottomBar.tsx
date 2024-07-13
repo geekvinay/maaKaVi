90b512bbb08b34972db4f59369e62ee36d833acd
geekvinay <geekvinay002@gmail.com>
2024-07-13 19:02:14 +0530

    feat: working on the code editor

--- client/src/components/EditorComp/BottomBar/BottomBar.tsx
+++ client/src/components/EditorComp/BottomBar/BottomBar.tsx
@@ -1,10 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SettingType } from "../../../common/EditorTypes";
-import { useEffect } from "react";
 
 const BottomBar = ({ settings, setSettings }: { settings: SettingType, setSettings: any; }) => {
-    useEffect(() => {
-    });
     return (
         <div className="absolute bottom-0 w-screen left-0 h-[2rem] border-t-2 base-h-bg flex justify-between items-center px-[2rem] list-none">
             <li className="px-2 hover:cursor-pointer mr-auto">InBrowserIDE</li>
